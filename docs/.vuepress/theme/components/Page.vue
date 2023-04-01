59b51d2d398927f7dbb06537a14bff40d4c7bd8d
hfllove <1669471474@qq.com>
2023-04-01 10:12:26 +0800

    提交更新

--- docs/.vuepress/theme/components/Page.vue
+++ docs/.vuepress/theme/components/Page.vue
@@ -34,7 +34,7 @@ export default {
     background-size: 23px 23px;
     background-position: top left;
     // background-color: #f2f3f5;
-    
+    background-color: #C7EDCC;
 
 .page-main 
     display: block; 
@@ -48,7 +48,9 @@ export default {
     background-color: rgba(66, 66, 66, 0.9) !important;
     color:#eee;
     // font-family: "PingFang SC", "Hiragino Sans GB", "Heiti SC", "Microsoft YaHei", "WenQuanYi Micro Hei";
-    font: 16px/1.5 "Helvetica Neue",Helvetica,Arial,"Microsoft Yahei","Hiragino Sans GB","Heiti SC","WenQuanYi Micro Hei",sans-serif;
+    font: 16px/1.7 "Helvetica Neue",Helvetica,Arial,"Microsoft Yahei","Hiragino Sans GB","Heiti SC","WenQuanYi Micro Hei",sans-serif;
+    -webkit-text-size-adjust: 100%;
+    --wp--preset--font-size--normal: 16px;
 
 .page-container
     --color-background: #282828;
@@ -86,14 +88,6 @@ export default {
     --wp--preset--gradient--pale-ocean: linear-gradient(135deg,rgb(255,245,203) 0%,rgb(182,227,212) 50%,rgb(51,167,181) 100%);
     --wp--preset--gradient--electric-grass: linear-gradient(135deg,rgb(202,248,128) 0%,rgb(113,206,126) 100%);
     --wp--preset--gradient--midnight: linear-gradient(135deg,rgb(2,3,129) 0%,rgb(40,116,252) 100%);
-    --wp--preset--duotone--dark-grayscale: url(#wp-duotone-dark-grayscale);
-    --wp--preset--duotone--grayscale: url(#wp-duotone-grayscale);
-    --wp--preset--duotone--purple-yellow: url(#wp-duotone-purple-yellow);
-    --wp--preset--duotone--blue-red: url(#wp-duotone-blue-red);
-    --wp--preset--duotone--midnight: url(#wp-duotone-midnight);
-    --wp--preset--duotone--magenta-yellow: url(#wp-duotone-magenta-yellow);
-    --wp--preset--duotone--purple-green: url(#wp-duotone-purple-green);
-    --wp--preset--duotone--blue-orange: url(#wp-duotone-blue-orange);
     --wp--preset--font-size--small: 13px;
     --wp--preset--font-size--medium: 20px;
     --wp--preset--font-size--large: 36px;
