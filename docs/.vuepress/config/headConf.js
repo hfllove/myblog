31bc843bfac5156c83bc69ee3dfb0bacc12381a4
hfllove <1669471474@qq.com>
2023-04-02 22:51:51 +0800

    提交更新

--- docs/.vuepress/config/headConf.js
+++ docs/.vuepress/config/headConf.js
@@ -1,6 +1,7 @@
 module.exports = [
     ['link', { rel: 'icon', href: '/logo.png' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta',{name:'viewport',content:'width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover'}],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
